06e94737ee4d140f3334e46ccc96a86b5f6cc5a1
victoracuna-cell <victor.acuna@haulmer.com>
2026-06-26 12:20:32 -0400

    fix: load UIcons via @import inside style tag

--- utils/styles.py
+++ utils/styles.py
@@ -1,8 +1,7 @@
 GLOBAL_CSS = """
-<link rel='stylesheet' href='https://cdn-uicons.flaticon.com/2.6.0/uicons-regular-rounded/css/uicons-regular-rounded.css'>
-<link rel='stylesheet' href='https://cdn-uicons.flaticon.com/2.6.0/uicons-solid-rounded/css/uicons-solid-rounded.css'>
 <style>
-
+@import url('https://cdn-uicons.flaticon.com/2.6.0/uicons-regular-rounded/css/uicons-regular-rounded.css');
+@import url('https://cdn-uicons.flaticon.com/2.6.0/uicons-solid-rounded/css/uicons-solid-rounded.css');
 @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap');
 
 /* ── Base ── */
